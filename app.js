36e9d59dd0195ad995e206f47735546797f1b32c
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2015-04-16 14:07:53 -0400

    use badgekit-api-client

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    Client = require('badgekit-api-client');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
+var apiEndpoint = process.env.BADGES_ENDPOINT || 'http://localhost:8080';
+var auth = {
+  key: process.env.BADGES_KEY || 'http://localhost:8080',
+  secret: process.env.BADGES_SECRET || 'http://localhost:8080'
+};
+
+var client = new Client(apiEndpoint, auth);
+
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
