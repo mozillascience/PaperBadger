c1539a2120d3bda26be4121f5da72fe69d89ea0b
Austin Davis-Richardson <harekrishna@gmail.com>
2015-06-04 22:32:11 -0400

    added basic API testing with supertest for #19

---
+++ test/api.js
@@ -0,0 +1,16 @@
+var assert = require('assert')
+var request = require('supertest')
+
+var config = {
+}
+
+var app = require('../src/app.js')(config)
+
+describe("PaperBadger", function() {
+  it("GET / fetches welcome page", function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /html/)
+      .expect(200, done)
+  })
+})
