71c35d1a575c3cb402f4b15fd332b99e6bb172e2
Austin Davis-Richardson <harekrishna@gmail.com>
2015-06-05 10:07:30 -0400

    added semicolons

--- test/api.js
+++ test/api.js
@@ -1,16 +1,15 @@
-var assert = require('assert')
-var request = require('supertest')
+var assert = require('assert');
+var request = require('supertest');
 
-var config = {
-}
+var config = {};
 
-var app = require('../src/app.js')(config)
+var app = require('../src/app.js')(config);
 
-describe("PaperBadger", function() {
-  it("GET / fetches welcome page", function(done) {
+describe("PaperBadger", function () {
+  it("GET / fetches welcome page", function (done) {
     request(app)
       .get('/')
       .expect('Content-Type', /html/)
-      .expect(200, done)
-  })
-})
+      .expect(200, done);
+  });
+});
