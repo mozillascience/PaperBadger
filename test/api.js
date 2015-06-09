18f8ae478b50feaf8665540f5cad20c1f8c4cc0a
Austin Davis-Richardson <harekrishna@gmail.com>
2015-06-09 18:54:52 -0400

    fixed tests to work with Habitat

--- test/api.js
+++ test/api.js
@@ -1,23 +1,22 @@
 var request = require('supertest');
 
-// provide a session secret for testing
-var config = {
-  SESSION_SECRET: 'test_secret',
-  BADGES_ENDPOINT: 'http://example.com',
-  BADGES_SYSTEM: 'badgekit',
-  BADGES_KEY: 'test_key',
-  BADGES_SECRET: 'test_secret'
-}
+// test config (need to override .env and .env.dist)
+process.env.SESSION_SECRET        = 'test_secret'
+process.env.BADGES_ENDPOINT       = 'http://example.com'
+process.env.BADGES_SYSTEM         = 'badgekit'
+process.env.BADGES_KEY            = 'test_key'
+process.env.BADGES_SECRET         = 'test_secret'
+process.env.ORCID_AUTH_CLIENT_ID  = 'science'
+process.env.ORCID_AUTH_SITE       = 'http://example.com'
+process.env.ORCID_AUTH_TOKEN_PATH = 'http://api.example.com/oauth/token'
+process.env.ORCID_REDIRECT_URI    = 'http://localhost:5000/orcid_auth_callback'
 
-var app = require('../src/app.js')(config);
+var app = require('../src/app.js')();
 
 // setup nock mocking of badges endpoint
 var nock = require('nock');
 
-var scope = nock('http://example.com')
-  .log(console.log)
-  .get('/systems/badgekit/badges?archived=any')
-  .reply(200, []);
+var scope = nock('http://example.com');
 
 describe("PaperBadger", function () {
 
@@ -31,6 +30,10 @@ describe("PaperBadger", function () {
 
   // route: /badges
   it("GET /badges returns list of badges", function (done) {
+
+    scope.get('/systems/badgekit/badges?archived=any')
+      .reply(200, []);
+
     request(app)
       .get('/badges')
       .expect('Content-Type', /json/)
