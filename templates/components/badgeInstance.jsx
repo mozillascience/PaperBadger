074cd94ee28db277acacffd5ec7560181d770fd3
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2015-07-27 16:00:52 -0400

    Add some context at the top of each page * link out to ORCID / paper page if applicable

--- templates/components/badgeInstance.jsx
+++ templates/components/badgeInstance.jsx
@@ -4,13 +4,18 @@ var React = require('react');
 var BadgeInstance = React.createClass({
   render: function() {
     var badge = this.props.badge,
-        slug = "/badges/" + badge.slug + '?pretty=true';
+        doiRe = /(10\.\d{3}\d+)\/(.*)\b/,
+        m = doiRe.exec(badge.evidenceUrl),
+        paperUrl = '/v/#/papers/' + m[1] + '/' + encodeURIComponent(m[2]) + '/badges';
+
     return (
       <div className="badge pure-u-1-4">
-          <img src={ badge.badge.imageUrl }/>
+          <div className="pure-u-1-2">
+            <img src={ badge.badge.imageUrl }/>
+          </div>
           <h4><a href={ '/v/#/badges/' + badge.badge.slug }>{ badge.badge.name }</a></h4>
           <a href={ '/v/#/users/' + badge.orcid + '/badges'}> { badge.orcid } </a><br />
-          <a href={ badge.evidenceUrl } > { badge.evidenceUrl } </a>
+          <a href={ paperUrl } > { badge.evidenceUrl } </a>
       </div>
     );
   }
