4e39aed3d377e905e8f305bc6bb6b89456a587de
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2015-08-11 16:03:51 -0400

    badge -> badgeInstance * better description for variable

--- templates/components/badgeInstance.jsx
+++ templates/components/badgeInstance.jsx
@@ -3,19 +3,19 @@ var React = require('react');
 
 var BadgeInstance = React.createClass({
   render: function() {
-    var badge = this.props.badge,
+    var badgeInstance = this.props.badge,
         doiRe = /(10\.\d{3}\d+)\/(.*)\b/,
-        m = doiRe.exec(badge.evidenceUrl),
+        m = doiRe.exec(badgeInstance.evidenceUrl),
         paperUrl = '/v/#/papers/' + m[1] + '/' + encodeURIComponent(m[2]) + '/badges';
 
     return (
       <div className="badge pure-u-1-4">
           <div className="pure-u-1-2">
-            <img src={ badge.badge.imageUrl }/>
+            <img src={ badgeInstance.badge.imageUrl }/>
           </div>
-          <h4><a href={ '/v/#/badges/' + badge.badge.slug }>{ badge.badge.name }</a></h4>
-          <a href={ '/v/#/users/' + badge.orcid + '/badges'}> { badge.orcid } </a><br />
-          <a href={ paperUrl } > { badge.evidenceUrl } </a>
+          <h4><a href={ '/v/#/badges/' + badgeInstance.badge.slug }>{ badgeInstance.badge.name }</a></h4>
+          <a href={ '/v/#/users/' + badgeInstance.orcid + '/badges'}> { badgeInstance.orcid } </a><br />
+          <a href={ paperUrl } > { badgeInstance.evidenceUrl } </a>
       </div>
     );
   }
