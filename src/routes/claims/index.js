255d671d5fc4d65e705b8c7efc8ba0bbd1d711de
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2015-09-01 14:27:58 -0400

    claim codes

---
+++ src/routes/claims/index.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Claim = mongoose.model('Claim');
+
+function getClaim(request, response) {
+  if (!request.params.slug) {
+    response.status(400).end();
+    return;
+  }
+  var query  = Claim.where({ slug: request.params.slug });
+  query.findOne(function(err, claim){
+    response.json(claim);
+  })
+}
+
+module.exports = function () {
+  return {
+    // GET
+    getClaim: getClaim
+  };
+};
