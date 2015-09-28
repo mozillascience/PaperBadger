4790e5c248dbdc6b8c97c80887b977623b1b4886
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2015-09-27 19:11:03 -0600

    Add Ubiquity Press * to README, footer & about page

--- templates/pages/about.jsx
+++ templates/pages/about.jsx
@@ -11,7 +11,7 @@ var About = React.createClass({
         <h1>About Contributorship Badges</h1>
         <p>Exploring the use of digital badges for crediting contributors to scholarly papers for their work</p>
         <p>As the research environment becomes more digital, we want to test how we can use this medium to help bring transparency and credit for individuals in the publication process.</p>
-        <p>This work is a collaboration with publishers <a href="http://www.biomedcentral.com/">BioMed Central</a> (BMC) and the <a href="http://www.plos.org/">Public Library of Science</a> (PLoS); the biomedical research foundation, <a href="http://www.wellcome.ac.uk/">The Wellcome Trust</a>; the software and technology firm Digital Science; the registry of unique researcher identifiers, <a href="http://orcid.org/">ORCID</a>; and the <a href="http://mozillascience.org/">Mozilla Science Lab</a>.</p>
+        <p>This work is a collaboration with publishers <a href="http://www.biomedcentral.com/">BioMed Central</a> (BMC), <a href="http://www.ubiquitypress.com/">Ubiquity Press</a> (UP) and the <a href="http://www.plos.org/">Public Library of Science</a> (PLoS); the biomedical research foundation, <a href="http://www.wellcome.ac.uk/">The Wellcome Trust</a>; the software and technology firm Digital Science; the registry of unique researcher identifiers, <a href="http://orcid.org/">ORCID</a>; and the <a href="http://mozillascience.org/">Mozilla Science Lab</a>.</p>
       </Page>
       );
   }
