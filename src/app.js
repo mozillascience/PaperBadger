c85869441b98430600eed786b6720202758ca7fb
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2015-11-15 18:01:54 -0500

    mongo -> redis * this fixed all my weird session problems * ... not sure why

--- src/app.js
+++ src/app.js
@@ -69,13 +69,12 @@ var oauth2 = require('simple-oauth2')(credentials);
 
 // TODO: review proper session use
 var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
+var RedisStore = require('connect-redis')(session);
 
 var sessionConfig = {
   secret: env.get('SESSION_SECRET'),
-  store: new MongoStore({
-    url: mongoUri,
-    autoReconnect: true
+  store: new RedisStore({
+    url: env.get('REDISCLOUD_URL') || "redis://127.0.0.1:6379/0"
   }),
   name: 'sid', // Generic - don't leak information
   proxy: true, // Trust the reverse proxy for HTTPS/SSL
