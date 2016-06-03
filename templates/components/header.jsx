19041b7f4f0e1911370cce9ba8c356f02bf79e13
Andrea Bedini <andrea.bedini@curtin.edu.au>
2016-06-03 11:52:09 +0800

    Upgrade react (3 of X)
    Run react-codemod/transforms/pure-component.js

--- templates/components/header.jsx
+++ templates/components/header.jsx
@@ -3,22 +3,20 @@ var React = require('react'),
 
 
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="header">
-        <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
-          <Link className="pure-menu-heading" to="home">Paper Badger</Link>
+function Header(props) {
+  return (
+    <div className="header">
+      <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <Link className="pure-menu-heading" to="home">Paper Badger</Link>
 
-          <ul className="pure-menu-list">
-              <li className="pure-menu-item pure-menu-selected"><Link to="home" className="pure-menu-link">Home</Link></li>
-              <li className="pure-menu-item"><Link to="about" className="pure-menu-link">About</Link></li>
-              <li className="pure-menu-item"><Link to="issue" className="pure-menu-link">Issue</Link></li>
-          </ul>
-      </div>
-      </div>
-    );
-  }
+        <ul className="pure-menu-list">
+            <li className="pure-menu-item pure-menu-selected"><Link to="home" className="pure-menu-link">Home</Link></li>
+            <li className="pure-menu-item"><Link to="about" className="pure-menu-link">About</Link></li>
+            <li className="pure-menu-item"><Link to="issue" className="pure-menu-link">Issue</Link></li>
+        </ul>
+    </div>
+    </div>
+  );
 }
 
 module.exports = Header;
--- templates/components/page.jsx
+++ templates/components/page.jsx
@@ -5,40 +5,38 @@ var React = require('react'),
 
 
 
-class Page extends React.Component {
-  render() {
-    var splash;
+function Page(props) {
+  var splash;
 
-    if (this.props.splash){
-      splash =
-          <div className="splash-container">
-            <div className="splash">
-              <div className="splash-head">
-                Contributor Badges
-              </div>
-              <div className="splash-subhead">
-                <p>Exploring the use of digital badges for crediting contributors to scholarly papers for their work</p>
-              </div>
-              <p>
-              <Link to="about" className="pure-button pure-button-primary">Learn More</Link>
-              </p>
+  if (props.splash){
+    splash =
+        <div className="splash-container">
+          <div className="splash">
+            <div className="splash-head">
+              Contributor Badges
             </div>
+            <div className="splash-subhead">
+              <p>Exploring the use of digital badges for crediting contributors to scholarly papers for their work</p>
+            </div>
+            <p>
+            <Link to="about" className="pure-button pure-button-primary">Learn More</Link>
+            </p>
           </div>
-      }
-
-    return (
-      <div className={ this.props.splash ? "home" : "" }>
-        <Header />
-        { splash }
-        <div className="content-wrapper">
-          <div className="content">
-            { this.props.children }
-          </div>
-          <Footer />
         </div>
+    }
+
+  return (
+    <div className={ props.splash ? "home" : "" }>
+      <Header />
+      { splash }
+      <div className="content-wrapper">
+        <div className="content">
+          { props.children }
+        </div>
+        <Footer />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 module.exports = Page;
